c71abe6e545123a12cd30678839278d5a766c73b
Ana Turbiani <136373243+ana-turbiani@users.noreply.github.com>
2024-08-30 09:20:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -57,6 +57,13 @@ function showMessage() {
     document.getElementById("message").style.display = "block";
 }
 
+var audio = new Audio('audio.mp3');
+audio.addEventListener('canplaythrough', function() {
+  audio.play();
+});
+
+
+
 
 
 
